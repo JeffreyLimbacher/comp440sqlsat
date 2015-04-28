266450c6d7a99ccc4146fb3695e55ea1314633d5
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-04-28 11:40:48 -0700

    Get classes associated with track and test

---
+++ s15guest17/dbo/Stored Procedures/ClassesInTrack.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[ClassesInTrack]
+	@trackid int
+AS
+BEGIN
+	SELECT Class.ClassId, Class.Description, Class.Duration, Class.Topic, Person.FirstName, Person.LastName
+	FROM Class JOIN Track ON Class.TrackId = Track.TrackId
+			   LEFT JOIN Person ON Class.PersonId = Person.PersonId
+	WHERE Track.TrackId = @trackid
+END
+RETURN 0
---
+++ s15guest17/testClassInTrack.sql
@@ -0,0 +1,17 @@
+﻿
+BEGIN
+DECLARE @level int
+INSERT INTO Level(LevelDescription) VALUES ('hard af')
+SELECT @level=LevelId FROM Level WHERE LevelDescription LIKE 'hard af'
+
+INSERT INTO Person(FirstName, LastName) VALUES ('Pauline', 'Nazareno')
+DECLARE @teacher int
+
+SELECT @teacher=PersonId FROM Person WHERE FirstName LIKE 'Pauline'
+
+INSERT INTO Class(Topic, Description, PersonId, TrackId, LevelId) VALUES ('Teaching children SQL', 'Learn to teach children SQL', @teacher, 1, @level), ('SQLite is dumb', 'Why SQLite is dumb', @teacher, 1, @level)
+
+
+END
+
+exec ClassesInTrack 1
