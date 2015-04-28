0b5e8e4e0bb47adf98d9c4039fc762b5d610356e
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-04-28 13:13:02 -0700

    Change event to have start date and end date

--- s15guest17/dbo/Tables/Event.sql
+++ s15guest17/dbo/Tables/Event.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[Event] (
     [EventId]    INT       NOT NULL,
-    [Date]       CHAR (18) NULL,
+    [StartDate]       DATE NOT NULL,
     [LocationId] INT       NOT NULL,
+    [EndDate] DATE NOT NULL, 
     CONSTRAINT [XPKEvent] PRIMARY KEY CLUSTERED ([EventId] ASC),
     CONSTRAINT [R_33] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Location] ([LocationId])
 );
