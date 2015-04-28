04eec485f235de429c277b83717921a12aa841a1
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-04-28 13:19:10 -0700

    add session

--- s15guest17/dbo/Stored Procedures/AddSession.sql
+++ s15guest17/dbo/Stored Procedures/AddSession.sql
@@ -1,6 +1,30 @@
 ﻿CREATE PROCEDURE [dbo].[AddSession]
-	@param1 int = 0,
-	@param2 int
+	@starttime datetime = 0,
+	@eventid int,
+	@roomid int,
+	@classid int
 AS
-	SELECT @param1, @param2
+BEGIN
+	SET NOCOUNT ON
+	BEGIN TRY
+		INSERT INTO Session(StartTime, EventId, RoomId, ClassId) VALUES (@starttime, @eventid, @roomid, @classid)
+	END TRY
+	BEGIN CATCH
+		DECLARE @ErrorMessage NVARCHAR(4000);
+			DECLARE @ErrorSeverity INT;
+			DECLARE @ErrorState INT;
+
+			SELECT @ErrorMessage = ERROR_MESSAGE(),
+				   @ErrorSeverity = ERROR_SEVERITY(),
+				   @ErrorState = ERROR_STATE();
+
+			-- Use RAISERROR inside the CATCH block to return 
+			-- error information about the original error that 
+			-- caused execution to jump to the CATCH block.
+			RAISERROR (@ErrorMessage, -- Message text.
+					   @ErrorSeverity, -- Severity.
+					   @ErrorState -- State.
+				   );
+	END CATCH
+END
 RETURN 0
