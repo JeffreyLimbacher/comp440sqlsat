0072ffb2192a66e2e4f0a03419bbe8f002f91d5f
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-04-28 13:21:19 -0700

    Allow null into location

--- s15guest17/dbo/Tables/Event.sql
+++ s15guest17/dbo/Tables/Event.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Event] (
     [EventId]    INT       NOT NULL IDENTITY,
     [StartDate]       DATE NOT NULL,
-    [LocationId] INT       NOT NULL,
+    [LocationId] INT       NULL,
     [EndDate] DATE NOT NULL, 
     CONSTRAINT [XPKEvent] PRIMARY KEY CLUSTERED ([EventId] ASC),
     CONSTRAINT [R_33] FOREIGN KEY ([LocationId]) REFERENCES [dbo].[Location] ([LocationId])
