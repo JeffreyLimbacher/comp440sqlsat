69a34ea14a942e2eb5dc3aac8cc562f567d401e7
Jeffrey Limbacher <jeffreylimbacher@gmail.com>
2015-04-29 18:35:06 -0700

    create role table and connect to rest of db

--- s15guest17/dbo/Tables/PersonSession.sql
+++ s15guest17/dbo/Tables/PersonSession.sql
@@ -1,11 +1,13 @@
-﻿CREATE TABLE [dbo].[Students] (
+﻿CREATE TABLE [dbo].[PersonSession] (
     [SessionId] INT      NOT NULL,
     [Grade]     CHAR (2) NULL,
     [PersonId]  INT      NOT NULL,
+    [RoleId] INT NOT NULL, 
     CONSTRAINT [XPKStudents] PRIMARY KEY CLUSTERED ([SessionId] ASC, [PersonId] ASC),
     CONSTRAINT [R_11] FOREIGN KEY ([SessionId]) REFERENCES [dbo].[Session] ([SessionId]),
     CONSTRAINT [R_27] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId]),
     CONSTRAINT [R_29] FOREIGN KEY ([SessionId]) REFERENCES [dbo].[Session] ([SessionId]),
-    CONSTRAINT [R_5] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId])
+    CONSTRAINT [R_5] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([PersonId]), 
+    CONSTRAINT [FK_PersonSession_ToRoles] FOREIGN KEY ([RoleId]) REFERENCES [Role]([RoleId])
 );
 
